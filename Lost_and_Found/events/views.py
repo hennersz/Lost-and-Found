9a83fc687eb021678562cbd0df7259850faf2104
Axel Goetz <axel.goez@hotmail.com>
2015-10-24 20:12:43 +0100

    Updated login page

--- Lost_and_Found/events/views.py
+++ Lost_and_Found/events/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+from django.conf import settings
+from django.shortcuts import redirect
+
+#Other functions
+def logUserIn(username, password):
+	user = authenticate(username=username, password=password)
+	if user is not None:
+		if user.is_active:
+			login(request, user)
+			return redirect('events/index.html')
+		else:
+			error_message = "The password is valid, but the account has been disabled!"
+			return render(request, 'events/login.html')
+	else:
+		error_message = "The password is not correct, try again"
+		return render(request, 'events/login.html')
 
 # Create your views here.
 def index(request):
@@ -15,14 +33,7 @@ def eventDetail(request, eventID):
 def login(request):
 	username = request.POST['username']
 	password = request.POST['password']
-	#if both nul, then display login
-	user = authenticate(username=username, password=password)
-	if user is not None:
-		if user.is_active:
-			login(request, user)
-		else:
-			# Return a 'disabled account' error message
-			login(request, user)
-	else:
-		# Return an 'invalid login' error message.
-		login(request, user)
+	if username == "" and password == "":
+		return render(request, 'events/login.html')
+
+	return logUserIn(username, password)
