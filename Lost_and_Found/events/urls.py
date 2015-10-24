8079f5e9b0ed156fc6731f3e094487562a504e0d
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-10-24 17:55:32 +0100

    test views and tempaltes

---
+++ Lost_and_Found/events/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^events/$', views.event, name='event'),
+    url(r'^events/(?P<eventID>[0-9]+)/$', views.eventDetail, name='eventDetail'),
+]
--- Lost_and_Found/events/views.py
+++ Lost_and_Found/events/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+	return HttpResponse("hello, world!")
+
+def event(request):
+	return HttpResponse("all events")
+
+def eventDetail(request, eventID):
+	return HttpResponse("viewing event %s" % eventID)
